697e93a8aea8cecb3fcb38224c9153cbc5953011
maaelattar <41055637+maaelattar@users.noreply.github.com>
2020-07-11 23:55:36 +0200

    Add navbar component logic and style

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './navbar.scss';
+export default function Navbar() {
+	return (
+		<nav className='navbar'>
+			<img src={logo} alt='city tours company' />
+			<ul className='nav-links'>
+				<li>
+					<a className='nav-link' href=''>
+						home
+					</a>
+				</li>
+				<li>
+					<a className='nav-link' href=''>
+						about
+					</a>
+				</li>
+				<li>
+					<a className='nav-link active' href=''>
+						tours
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
