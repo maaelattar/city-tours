d2b59bd497dec4fb1dff41b28d8427965a072d58
maaelattar <41055637+maaelattar@users.noreply.github.com>
2020-07-12 01:18:57 +0200

    Add TourList Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
 function App() {
 	return (
 		<React.Fragment>
 			<Navbar />
+			<TourList />
 		</React.Fragment>
 	);
 }
